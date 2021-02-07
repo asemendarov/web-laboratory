403f3d3244a6f712602a289e815dd0ff19761104
oNoComments-1 <31689842+oNoComments@users.noreply.github.com>
2021-02-07 18:17:00 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="app">
+    <json-reader />
+  </div>
+</template>
+
+<script>
+import JsonReader from './components/JsonReader'
+
+export default {
+  name: 'App',
+  components: {
+    JsonReader
+  }
+}
+</script>
+
+<style>
+@import url(https://necolas.github.io/normalize.css/8.0.1/normalize.css);
+
+*,
+h1,
+h2,
+h3 {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+:root {
+  font-size: 12px;
+}
+
+body {
+  height: 100%;
+
+  display: flex;
+  flex-direction: column;
+}
+
+#app {
+  flex: 1 0 auto;
+  display: flex;
+
+  background-color: #222;
+  color: #eee;
+}
+</style>
---
+++ src/components/JsonReader.vue
@@ -0,0 +1,70 @@
+<template>
+  <div id="JsonReader">
+    <div class="folder-container">
+      <h1>Папка</h1>
+    </div>
+    <div class="file-container">
+      <h1>Файл</h1>
+    </div>
+    <div class="view-container">
+      <pre v-text="data"></pre>
+    </div>
+  </div>
+</template>
+
+<script>
+// import jsonData from '../json/data.json'
+
+export default {
+  name: 'JsonReader',
+  data() {
+    return {
+      data: null
+    }
+  },
+  mounted() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((response) => response.json())
+      .then((json) => (this.data = json))
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+#JsonReader {
+  width: 100%;
+  display: flex;
+}
+
+#JsonReader > div {
+  border: 1px solid black;
+}
+
+.folder-container {
+  min-width: 120px;
+  /* max-width: 300px; */
+
+  background-color: #444;
+}
+
+.file-container {
+  min-width: 120px;
+  /* max-width: 300px; */
+
+  background-color: #666;
+}
+
+.view-container {
+  flex: 1 0 auto;
+
+  background-color: #888;
+}
+
+h1 {
+  padding: 10px;
+
+  text-align: center;
+  border-bottom: 1px dashed black;
+}
+</style>
