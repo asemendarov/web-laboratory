6a64522d5ac8eea3edbdca942d48b9f5cca629e9
oNoComments-1 <31689842+oNoComments@users.noreply.github.com>
2021-02-11 14:37:20 +0300

    Added information bar for files

--- src/components/JsonReader.vue
+++ src/components/JsonReader.vue
@@ -22,7 +22,7 @@
         </div>
       </div>
     </div>
-    <div class="file-container" v-if="isActiveFolder">
+    <div class="file-container" v-if="isVisibilityFolder">
       <div class="file-head">
         <h1>Файл</h1>
         <hr />
@@ -33,7 +33,7 @@
           v-for="file in files"
           :key="file[0]"
           :title="titleFormat(file[1])"
-          @click="showFile(file[1].url)"
+          @click="showFile(file[1].id)"
         >
           <span class="file-name" v-text="nameFormat(file[1].name)">Name File</span>
           <b-icon-arrow-right class="file-name-icon"></b-icon-arrow-right>
@@ -42,6 +42,19 @@
     </div>
     <div class="view-container">
       <component :is="FileComponent" v-bind="PropsComponent"></component>
+    </div>
+
+    <div class="mini-info" @click="calcVisibilityInfo">Информация</div>
+    <div class="info-container" v-if="!isVisibilityInfo">
+      <div class="info-head">
+        <h1>Информация</h1>
+        <hr />
+      </div>
+      <div class="info-body">
+        <div class="info-body-content" v-for="(value, index) of infoList" :key="index">
+          {{ index }} : {{ value }}
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -67,8 +80,13 @@ export default {
       folders: new Map(),
       files: new Map(),
 
-      isActiveFolder: false,
+      infoList: null,
+
+      isVisibilityInfo: false,
+      isVisibilityFolder: false,
+
       idFocusFolder: null,
+      idFocusFile: null,
 
       FileComponent: null,
       PropsComponent: null
@@ -77,6 +95,20 @@ export default {
 
   created() {
     this.parseStructure(this.data)
+  },
+
+  watch: {
+    idFocusFile(newId, oldId) {
+      console.log('newId', newId)
+
+      if (!newId || !this.files.has(newId)) {
+        return { info: 'Необходимо выбрать файл' }
+      }
+
+      console.log('Файл найден', newId)
+
+      this.infoList = this.files.get(newId)
+    }
   },
 
   methods: {
@@ -118,26 +150,33 @@ export default {
       this.updateVisibilityFiles(folder.id)
     },
 
-    // @return -- true (enabled) / false (disabled)
     updateVisibilityFiles(idFolder) {
       if (this.idFocusFolder === idFolder) {
-        this.isActiveFolder = !this.isActiveFolder
+        this.isVisibilityFolder = !this.isVisibilityFolder
         this.idFocusFolder = null
       } else {
-        this.isActiveFolder = false
-        this.isActiveFolder = true
+        this.isVisibilityFolder = true
         this.idFocusFolder = idFolder
       }
     },
 
-    showFile(url) {
-      if (/^.*\.(pdf|PDF)$/.test(url)) {
+    // удалить url <<--------------------------------------------------
+    showFile(id) {
+      const file = this.files.get(id)
+      if (!file) return
+
+      this.idFocusFile = file.id
+
+      if (/^.*\.(pdf|PDF)$/.test(file.url)) {
         this.FileComponent = 'pdf'
-        this.PropsComponent = { src: url }
+        this.PropsComponent = {
+          src: file.url,
+          style: 'display: block; width: 100%'
+        }
       }
 
-      if (/^.*\.(xlsx|xls|xlsm)$/.test(url)) {
-        this.fetchXLS(url)
+      if (/^.*\.(xlsx|xls|xlsm)$/.test(file.url)) {
+        this.fetchXLS(file.url)
           .then((json) => {
             this.FileComponent = 'vue-table'
             this.PropsComponent = {
@@ -151,6 +190,10 @@ export default {
             console.log(err)
           })
       }
+    },
+
+    calcVisibilityInfo() {
+      this.isVisibilityInfo = !this.isVisibilityInfo
     },
 
     fetchXLS(url) {
@@ -221,22 +264,40 @@ export default {
 }
 
 .folder-container,
-.file-container {
+.file-container,
+.info-container {
   width: 200px;
+  min-width: 200px;
   border: 1px solid #0b5aa2;
   border-top: 10px solid #0b5aa2;
   background: #fffffe;
 }
 
+.info-container {
+  width: 250px;
+  min-width: 250px;
+}
+
+.mini-info {
+  text-transform: lowercase;
+  writing-mode: vertical-rl;
+  color: #eee;
+  background-color: #0b5aa2;
+  padding-top: 32px;
+  cursor: pointer;
+}
+
 .folder-container h1,
-.file-container h1 {
+.file-container h1,
+.info-container h1 {
   padding: 5px;
   margin: 10px;
   color: #0b5aa2;
 }
 
 .folder-container hr,
-.file-container hr {
+.file-container hr,
+.info-container hr {
   width: 40%;
   margin: 0px 15px;
   border: 1px solid #0b5aa2;
@@ -244,7 +305,8 @@ export default {
 }
 
 .folder-body,
-.file-body {
+.file-body,
+.info-body {
   margin: 10px;
   display: grid;
   grid-gap: 10px;
@@ -259,6 +321,12 @@ export default {
   grid-gap: 10px;
   border: 1px solid transparent;
   cursor: pointer;
+}
+
+.info-body-content {
+  width: 100%;
+  word-break: break-all;
+  border: 1px solid transparent;
 }
 
 .folder-body-content:hover,
@@ -281,14 +349,13 @@ export default {
 }
 
 .view-container {
-  flex: 1 0 auto;
+  flex: 1 0;
 }
 
 table {
   border-collapse: collapse;
   border: 1px solid #eee;
   width: 100%;
-  margin-bottom: 20px;
 }
 table >>> th {
   font-weight: bold;
