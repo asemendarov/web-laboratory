0438b3b0551d44a60095bf4173e5b7e230b12509
oNoComments-1 <31689842+oNoComments@users.noreply.github.com>
2021-03-03 17:46:27 +0300

    Ex5: Fix tailwind config

--- experiment-5/tailwind.config.js
+++ experiment-5/tailwind.config.js
@@ -4,21 +4,41 @@ module.exports = {
       content: ['./public/**/*.html', './src/**/*.vue']
   },
   theme: {
-    'sm': '640px',
-    // => @media (min-width: 640px) { ... }
+    screens: {
+      // 'sm': '640px',
+      // // => @media (min-width: 640px) { ... }
 
-    // 'md': '768px', // default
-    'md': '808px', // my
-    // => @media (min-width: 768px) { ... }
+      // // 'md': '768px', // default
+      // 'md': '808px', // my
+      // // => @media (min-width: 768px) { ... }
 
-    'lg': '1024px',
-    // => @media (min-width: 1024px) { ... }
+      // 'lg': '1024px',
+      // // => @media (min-width: 1024px) { ... }
 
-    'xl': '1280px',
-    // => @media (min-width: 1280px) { ... }
+      // 'xl': '1280px',
+      // // => @media (min-width: 1280px) { ... }
 
-    '2xl': '1536px',
-    // => @media (min-width: 1536px) { ... }
+      // '2xl': '1536px',
+      // // => @media (min-width: 1536px) { ... }
+      
+      // ----------------------------------------
+
+      '2xl': {'max': '1535px'},
+      // => @media (max-width: 1535px) { ... }
+
+      'xl': {'max': '1279px'},
+      // => @media (max-width: 1279px) { ... }
+
+      'lg': {'max': '1023px'},
+      // => @media (max-width: 1023px) { ... }
+
+      // 'md': {'max': '767px'}, // default
+      'md': {'max': '808px'}, // my
+      // => @media (max-width: 767px) { ... }
+
+      'sm': {'max': '639px'},
+      // => @media (max-width: 639px) { ... }
+    },
   },
   variants: {},
   plugins: [],
