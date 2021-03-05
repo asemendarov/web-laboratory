e6130f38f66f88fdd693f427290ab6425e635041
oNoComments-1 <31689842+oNoComments@users.noreply.github.com>
2021-03-05 16:59:11 +0300

    Ex5: Hotfix #5120b20 #f953c9f

--- experiment-5/src/App.vue
+++ experiment-5/src/App.vue
@@ -22,13 +22,13 @@ export default {
   -o-overflow-scrolling: touch
 
   scrollbar-width: 7px
-  scrollbar-color: var(#30363d)
+  scrollbar-color: #30363d
 
-  scrollbar-face-color: var(#30363d)
-  scrollbar-shadow-color: var(#30363d)
-  scrollbar-highlight-color: var(#30363d)
+  scrollbar-face-color: #30363d
+  scrollbar-shadow-color: #30363d
+  scrollbar-highlight-color: #30363d
 
-  @apply tw-overflow-y-auto tw-overflow-y-hidden
+  @apply tw-overflow-y-auto tw-overflow-x-hidden
 
   &::-webkit-scrollbar
     -webkit-appearance: none
@@ -36,7 +36,7 @@ export default {
 
   &::-webkit-scrollbar-thumb
     border-radius: 3px
-    background-color: var(#30363d)
-    box-shadow: 0 0 1px var(#30363d)
-    -webkit-box-shadow: 0 0 1px var(#30363d)
+    background-color: #30363d
+    box-shadow: 0 0 1px #30363d
+    -webkit-box-shadow: 0 0 1px #30363d
 </style>
