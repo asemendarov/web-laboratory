159d3eeb496d06ba9f139096f8b5f6a1c3936720
oNoComments-1 <31689842+oNoComments@users.noreply.github.com>
2021-03-05 16:50:39 +0300

    Ex5: Code refactoring $footer

--- experiment-5/src/components/$footer/AppFooter.vue
+++ experiment-5/src/components/$footer/AppFooter.vue
@@ -1,24 +1,24 @@
 <template>
-  <div class="app-footer tw-relative footer-wrapper">
-    <footer class="footer tw-flex tw-px-8 tw-py-4 __md__ md:tw-flex-col md:tw-items-center md:tw-justify-center">
-      <ul class="ul tw-flex tw-items-center tw-justify-between __md__ md:tw-items-center md:tw-justify-center md:tw-flex-full">
-        <li class="li tw-flex tw-items-center tw-justify-center __md__ md:tw-mr-4">
+  <div class="app-footer">
+    <footer class="footer">
+      <ul class="ul footer-item">
+        <li class="li">
           <span class="tw-whitespace-nowrap">© 2021 Alpha Centauri</span>
         </li>
-        <li class="li tw-flex tw-items-center tw-justify-center __md__ md:tw-mr-4"><router-link to="">API</router-link></li>
-        <li class="li tw-flex tw-items-center tw-justify-center __md__ md:tw-mr-4">
+        <li class="li"><router-link to="">API</router-link></li>
+        <li class="li">
           <router-link to="">Codex</router-link>
         </li>
       </ul>
-      <app-logo class="footer-item footer-logo tw-flex tw-items-center tw-justify-center tw-flex-auto __md__ md:tw-hidden" width="32px" height="32px" :full="false" alpha="55" />
-      <ul class="ul tw-flex tw-items-center tw-justify-between __md__ md:tw-items-center md:tw-justify-center md:tw-flex-full">
-        <li class="li tw-flex tw-items-center tw-justify-center __md__ md:tw-mr-4">
+      <app-logo class="footer-logo footer-item" width="32px" height="32px" :full="false" alpha="55" />
+      <ul class="ul footer-item">
+        <li class="li">
           <router-link to="">Contact</router-link>
         </li>
-        <li class="li tw-flex tw-items-center tw-justify-center __md__ md:tw-mr-4">
+        <li class="li">
           <router-link to="">Privacy</router-link>
         </li>
-        <li class="li tw-flex tw-items-center tw-justify-center __md__ md:tw-mr-4">
+        <li class="li">
           <router-link :to="{ name: 'About' }">About</router-link>
         </li>
       </ul>
@@ -40,20 +40,27 @@ export default {
 </script>
 <style lang="sass" scoped>
 .app-footer
-  margin: 0 auto
-  max-width: 1280px
+  @apply tw-relative tw-mx-auto tw-my-0 tw-max-w-7xl
 
 .footer
-  margin-top: 40px
+  @apply tw-mt-10 tw-px-8 tw-py-4 tw-flex tw-border-t tw-border-very-dark-grayish-blue-700
+  @apply md:tw-flex-col md:tw-items-center md:tw-justify-center
 
-  border-top: 1px solid var(--color-scrollbar)
+  &-logo
+    @apply tw-flex tw-items-center tw-justify-center tw-flex-auto
+    @apply md:tw-hidden
 
 .ul
-  width: 40%
+  @apply tw-w-2/5 tw-flex tw-items-center tw-justify-between
+  @apply md:tw-items-center md:tw-justify-center md:tw-flex-full
+
+.li
+  @apply tw-flex tw-items-center tw-justify-center
+  @apply md:tw-mr-4
 
 a
-  color: var(--color-link-footer)
+  @apply tw-text-moderate-blue-500
 
-a:hover
-  text-decoration: underline
+  &:hover
+    @apply tw-underline
 </style>
